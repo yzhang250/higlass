fab8bb3af5d201b06cb7c9c0bd59f3dd9cecefb9
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-07-25 15:27:27 -0700

    Working on the bam file fetcher

--- app/scripts/data-fetchers/bam-fetcher.js
+++ app/scripts/data-fetchers/bam-fetcher.js
@@ -6,21 +6,21 @@ const getAlignments = (bamUrl, chromName, start, end) => {
 
 };
 
-const t = new BamFile({
-  bamUrl: 'https://pkerp.s3.amazonaws.com/public/bamfile_test/SRR1770413.sorted.bam',
-});
+// const t = new BamFile({
+//   bamUrl: 'https://pkerp.s3.amazonaws.com/public/bamfile_test/SRR1770413.sorted.bam',
+// });
 
-t.getHeader()
-  .then((header) => {
-    console.log('header:', header);
-    t.getRecordsForRange('Chromosome', 0, 1000)
-      .then((records) => {
-        for (let i = 0; i < 2; i++) {
-          console.log('records[i]', records[i].get('seq'));
-        }
-        console.log('records:', records);
-      });
-  });
+// t.getHeader()
+//   .then((header) => {
+//     console.log('header:', header);
+//     t.getRecordsForRange('Chromosome', 0, 1000)
+//       .then((records) => {
+//         for (let i = 0; i < 2; i++) {
+//           console.log('records[i]', records[i].get('seq'));
+//         }
+//         console.log('records:', records);
+//       });
+//   });
 
 
 class BAMDataFetcher {
@@ -28,15 +28,20 @@ class BAMDataFetcher {
     this.dataConfig = dataConfig;
     this.trackUid = slugid.nice();
 
+    this.bamFile = new BamFile({
+      bamUrl: dataConfig.url
+    });
 
     this.dataPromise = new Promise((resolve, reject) => {
       ChromosomeInfo(dataConfig.chromSizesUrl, resolve);
     });
 
-    this.dataPromise.then((chromInfo) => {
-      this.chromInfo = chromInfo;
-      console.log('chromInfo:', chromInfo);
-    });
+    Promise.all([this.dataPromise, this.bamFile])
+      .then((values) => {
+        // we need to load the headers for the bam file but we don't
+        // need to store them anywhere
+        this.chromInfo = values[0];
+      });
   }
 
   tilesetInfo(callback) {
@@ -97,39 +102,59 @@ class BAMDataFetcher {
   tile(z, x) {
     return this.tilesetInfo().then((tsInfo) => {
       const tileWidth = +tsInfo.max_width / 2 ** (+z);
+      const recordPromises = [];
 
       // get the bounds of the tile
-      const minX = tsInfo.min_pos[0] + x * tileWidth;
+      let minX = tsInfo.min_pos[0] + x * tileWidth;
       const maxX = tsInfo.min_pos[0] + (x + 1) * tileWidth;
 
       const chromLengths = this.chromInfo.chromLengths;
       const cumPositions = this.chromInfo.cumPositions;
 
-      let alignments = [];
+      const alignments = [];
 
       for (let i = 0; i < cumPositions.length; i++) {
         const chromName = cumPositions[i].chrom;
         const chromStart = cumPositions[i].pos;
+
+        console.log('cumPoss:', cumPositions[i]);
+        console.log('chromName:', chromName, 'chromLenghts:', chromLengths);
         const chromEnd = cumPositions[i].pos + chromLengths[chromName];
 
-
+        console.log('minX:', minX, 'maxX', maxX);
+        console.log('chromStart', chromStart, 'chromEnd:', chromEnd);
         if (chromStart <= minX
-          && minX <= chromEnd) {
-          // found the start position
+          && minX < chromEnd) {
+          // start of the visible region is within this chromosome
 
-          if (i === cumPositions.length - 1) {
-            // last chromosome
-            if (maxX > chromEnd) {
-              alignments = alignments.concat(
-                getAlignments(this.dataConfig.url, chromName,
-                  minX - chromStart, maxX - chromStart)
-              );
-            }
+          if (maxX > chromEnd) {
+            // the visible region extends beyond the end of this chromosome
+
+            // fetch from the start until the end of the chromosome
+            recordPromises.push(
+              this.bamFile.getRecordsForRange(
+                chromName, minX - chromStart, chromEnd - chromStart
+              )
+            );
+
+            // continue onto the next chromosome
+            minX = chromEnd;
+          } else {
+            // the end of the region is within this chromosome
+            recordPromises.push(
+              this.bamFile.getRecordsForRange(
+                chromName, minX - chromStart, maxX - chromStart
+              )
+            );
+
+            // end the loop because we've retrieved the last chromosome
+            break;
           }
         }
       }
 
-      return [];
+      console.log('recordPromises:', recordPromises);
+      return Promise.all(recordPromises);
     });
   }
 }
